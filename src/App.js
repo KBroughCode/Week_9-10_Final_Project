4d377b3050af161c9bb2fff5b804f16e2e38ddda
RoderickKing <rod2king@gmail.com>
2019-05-29 17:19:37 +0100

    Navbar additions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Component } from "react";
+import React from "react";
+import Navbar from "./NavBar";
+import "./navbar.css";
+import BlackJack from "./BlackJack/BlackJack";
+import Main from "./main";
 
-function App() {
-  return (
-    <h1>Casino-Royale</h1>
-  );
+//<Route exact path="/" component={App} />
+//<Route path="/blackjack" component={DummyBlackjack} />
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <React.Fragment>
+          <Navbar />
+          <Route exact path="/" component={Main} />
+          <Route path="/BlackJack" component={BlackJack} />
+        </React.Fragment>
+      </Router>
+    );
+  }
 }
-
 export default App;
--- src/BlackJack/BlackJack.js
+++ src/BlackJack/BlackJack.js
@@ -1,38 +1,36 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux';
-
-
+import { connect } from "react-redux";
 
 class BlackJack extends Component {
-
   componentDidMount() {
     this.props.getDeck();
   }
 
-  render(){
+  render() {
     return (
       <div className="App">
         <h1>CASINO BLACKJACK</h1>
       </div>
     );
   }
-
 }
 
-const mapDispatchToProps = (dispatch) => ({
+const mapDispatchToProps = dispatch => ({
   getDeck() {
     dispatch(() => {
       fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6`)
-      .then(res => res.json())
-      .then(deck => {
-        dispatch({
-          type: 'GET_DECK',
-          deck
-        })
-      })
-    })
+        .then(res => res.json())
+        .then(deck => {
+          dispatch({
+            type: "GET_DECK",
+            deck
+          });
+        });
+    });
   }
-})
+});
 
-
-export default connect(null, mapDispatchToProps)(App);
+export default connect(
+  null,
+  mapDispatchToProps
+)(BlackJack);

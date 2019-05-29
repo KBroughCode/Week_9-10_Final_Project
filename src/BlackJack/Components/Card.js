72070b75573e62c5b2632d5cadc44a0c038ef35d
bsharp807 <a0030619@0030619s-MacBook-Pro.local>
2019-05-29 15:16:21 +0100

    card.js added with class names

---
+++ src/BlackJack/Components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Card = (props) => {
+  return(
+    <div className="card">
+      <img src={props.image} alt={props.code}></img>
+      <div className="card-details">{props.value} of {props.suit}</div>
+    </div>
+  )
+}
+
+export default Card;

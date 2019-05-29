536675bfce2c7efea0ad39205868c3df8086a155
bsharp807 <a0030619@0030619s-MacBook-Pro.local>
2019-05-29 15:06:35 +0100

    fixed bug in reducer, syntax error line 4

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 
 import cardReducer from './card_reducer.js';
-import playerReducer from './shuffle_reducer.js';
+import playerReducer from './player_reducer.js';
 import dealerReducer from './dealer_reducer.js';
 
 export default combineReducers({

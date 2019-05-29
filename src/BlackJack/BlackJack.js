38b7a215efe9554c90631a2aac63cdf5c122dc71
bsharp807 <a0030619@0030619s-MacBook-Pro.local>
2019-05-29 14:06:26 +0100

    blackjack.js code added

---
+++ src/BlackJack/BlackJack.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { connect } from 'react-redux';
+
+
+
+class BlackJack extends Component {
+
+  componentDidMount() {
+    this.props.getDeck();
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>CASINO BLACKJACK</h1>
+      </div>
+    );
+  }
+
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  getDeck() {
+    dispatch(() => {
+      fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6`)
+      .then(res => res.json())
+      .then(deck => {
+        dispatch({
+          type: 'GET_DECK',
+          deck
+        })
+      })
+    })
+  }
+})
+
+
+export default connect(null, mapDispatchToProps)(App);

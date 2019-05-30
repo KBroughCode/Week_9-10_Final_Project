7594fc390b95a0d0c5efc1bfd275df15013c1f02
JamesBerry <a9883805@9883805s-MacBook-Pro.local>
2019-05-30 10:19:10 +0100

    can calculate hand values as intended

---
+++ src/BlackJack/Logic/HandValueLogic.js
@@ -0,0 +1,36 @@
+const cardValue = require('./cardValueLogic');
+
+ const evaluateCards=(cardArray) => {
+   let total=[0];
+   cardArray.forEach((card) => {
+     total.forEach((element,i) => {
+       if(card ==='ACE'){
+         total[i] = [element+1,element+11];
+       }else{
+         total[i] += cardValue[card];
+       };
+     });
+     total = total.flat();
+   });
+   total = removeDuplicates(total);
+   total = removeOver21(total);
+   return  total
+ };
+
+ const removeDuplicates=(array) => {
+   return [...new Set(array)];
+ }
+
+ const removeOver21=(array) => {
+   const newArray = []
+   array.forEach((element) => {
+     if(element<22){
+       newArray.push(element)
+     }
+   })
+   return newArray;
+ }
+
+
+
+ console.log( evaluateCards(['QUEEN','KING']) );

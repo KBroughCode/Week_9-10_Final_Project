23cb5cd950665c88526083ad300c5a07c7bfb7aa
bsharp807 <a0030619@0030619s-MacBook-Pro.local>
2019-05-31 12:39:38 +0100

    dealer cards test updated

--- src/BlackJack/__tests__/dealer_cards.test.js
+++ src/BlackJack/__tests__/dealer_cards.test.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import DealerCards from '../Components/Card.js';
+import DealerCards from '../Components/DealerCards.js';
 import {render, fireEvent, cleanup} from '@testing-library/react';
 
 afterEach(cleanup)
 
-describe('delear cards limited test due to reducer constraints', () => {
-  it('should render something', () => {
-    const {getByLabelText} = render(<DealerCards />)
+describe('delear cards test', () => {
+  it('should render an error with no cards', () => {
+    const {getByText} = render(<DealerCards cards={[]} shuffleDealer={jest.fn()}/>)
+    expect(getByText('Whoops! The Dealer dropped his cards! Click here to get him to pick them up')).toBeTruthy();
+  })
+
+  it('should render cards', () => {
+    const {getByText, getByLabelText} = render(<DealerCards cards={[{
+      image: "https://deckofcardsapi.com/static/img/AS.png",
+      value: "ACE",
+      suit: "SPADES",
+      code: "AS"
+    }
+  ]} shuffleDealer={jest.fn()}/>)
     expect(getByLabelText('image')).toBeTruthy();
+    expect(getByText('ACE of SPADES')).toBeTruthy();
   })
 })

9db881991eb49e650dd610b64d3a3760275778a8
bsharp807 <a0030619@0030619s-MacBook-Pro.local>
2019-05-31 12:10:05 +0100

    testing started on card.js

---
+++ src/BlackJack/__tests__/card.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Card from '../Components/Card.js';
+import {render, fireEvent, cleanup} from 'react-testing-library';
+
+afterEach(cleanup)
+
+describe('Card render test', () => {
+  it('should display an image of a card', () => {
+    const {getByLabelText} = render(<Card
+      code= "0D"
+      image= "https://deckofcardsapi.com/static/img/0D.png"
+      suit= "DIAMONDS"
+      value= "10"
+      />)
+      expect(getByLabelText('image')).toBeTruthy();
+  })
+})

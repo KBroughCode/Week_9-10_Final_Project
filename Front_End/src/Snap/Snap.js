2cb7f00a9599ff605dfa10b8f2c351f1436ddd3c
bsharp807 <a0030619@0030619s-MacBook-Pro.local>
2019-06-03 11:45:53 +0100

    snap start button created

--- Front_End/src/Snap/Snap.js
+++ Front_End/src/Snap/Snap.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
+import SnapStartButton from './Components/SnapStartButton'
 
 class Snap extends Component {
   constructor(props) {

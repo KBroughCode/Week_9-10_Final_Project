0e09fbf9e6a013340e950b0b449041bb3fd17554
bsharp807 <a0030619@0030619s-MacBook-Pro.local>
2019-06-03 10:43:41 +0100

    added coin display

--- Front_End/src/NavBar.js
+++ Front_End/src/NavBar.js
@@ -1,19 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { connect } from 'react-redux';
 import "./navbar.css";
 
-const Navbar = () => (
-
-  <ul className='navbar'>
-    <div className='link'>
-      <Link className='link' to="/">Home</Link>
+const Navbar = (props) => (
+  <div>
+    <div className='navbar'>
+    {props.coins}
+      <div className='link'>
+        <Link className='link' to="/">Home</Link>
+      </div>
+      <div className='link'>
+        <Link className='link' to="/Blackjack">Blackjack</Link>
+      </div>
     </div>
-    <div className='link'>
-      <Link className='link' to="/Blackjack">Blackjack</Link>
-    </div>
-  </ul>
-
+  </div>
 
 );
 
-export default Navbar;
+const mapStateToProps = (state) => {
+  return {
+      coins: state.coins
+    }
+}
+
+export default connect(mapStateToProps)(Navbar);

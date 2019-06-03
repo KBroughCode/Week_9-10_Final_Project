630d844594946239639d93202923903c3435ab51
bsharp807 <a0030619@0030619s-MacBook-Pro.local>
2019-06-03 13:32:02 +0100

    fixed refresh issue

--- Front_End/src/BlackJack/BlackJack.js
+++ Front_End/src/BlackJack/BlackJack.js
@@ -29,10 +29,6 @@ class BlackJack extends Component {
 
   handleGameStart() {
     this.setState({reveal: !this.state.reveal})
-    const cardFlip = document.querySelector(".start-game");
-      cardFlip.addEventListener("click", function() {
-      cardFlip.classList.toggle("flip");
-    })
   }
 
   render(){

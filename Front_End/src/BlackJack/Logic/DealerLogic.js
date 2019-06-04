e00c66ce1b469f916fc3e08f63ef13ba99831472
bsharp807 <a0030619@0030619s-MacBook-Pro.local>
2019-06-04 15:34:26 +0100

    removed commented out code from dealer logic

--- Front_End/src/BlackJack/Logic/DealerLogic.js
+++ Front_End/src/BlackJack/Logic/DealerLogic.js
@@ -13,19 +13,4 @@ const dealerHand=(cardArray) => {
   return {stick:stick,value:handValue}
 }
 
-// console.log(dealerHand([
-//   {
-//     image: "https://deckofcardsapi.com/static/img/KH.png",
-//     value: "KING",
-//     suit: "HEARTS",
-//     code: "KH"
-//   },
-//   {
-//     image: "https://deckofcardsapi.com/static/img/8C.png",
-//     value: "8",
-//     suit: "CLUBS",
-//     code: "8C"
-//   }
-// ]));
-
 module.exports = dealerHand;

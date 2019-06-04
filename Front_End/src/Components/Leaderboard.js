c8a774d5623b28469882ffaba4acfeb3d5ffd06b
bsharp807 <a0030619@0030619s-MacBook-Pro.local>
2019-06-04 16:30:04 +0100

    leaderboard now has effective infinite scroll

--- Front_End/src/Components/Leaderboard.js
+++ Front_End/src/Components/Leaderboard.js
@@ -2,15 +2,15 @@ import React from 'react';
 import './leaderboard.css';
 //Top 10 leaderboard
 const Leaderboard = (props) => {
-  const highboard = props.leaders.map((element, index) => {
-    while (index < 5) {
-      return ` ♣ ${element.name}: 	₡${element.score} ♣ `
-    }
+  const topTen = props.leaders.slice(0,10)
+  console.log(topTen);
+  const highboard = topTen.map((element, index) => {
+    return ` ♣ ${index+1}. ${element.name} - ₡${element.score} ♣ `
   })
   return (
 
-    <div className="scroll-left">
-     <p>High Scores {highboard}</p>
+    <div className="slideshow">
+     <p>High Scores {highboard} High Scores {highboard} High Scores {highboard}</p>
      </div>
     )
 

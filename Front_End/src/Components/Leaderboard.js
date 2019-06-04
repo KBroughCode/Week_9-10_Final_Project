a5e2d67360a66261d26a8a980219e31c52267012
RoderickKing <rod2king@gmail.com>
2019-06-04 11:27:10 +0100

    Scrolling leader v2

--- Front_End/src/Components/Leaderboard.js
+++ Front_End/src/Components/Leaderboard.js
@@ -1,60 +1,17 @@
 import React from 'react';
 import './leaderboard.css';
-
+//Top 10 leaderboard
 const Leaderboard = (props) => {
-
-  function generate_text() {
-
-  const topScores = props.leaders.length;
-  console.log('top score = ' , topScores);
-  switch (topScores) {
-    case 0:
-      return <div>No entries in high score table</div>
-
-    case 1:
-      return <div>{props.leaders[0].name} : {props.leaders[0].score} points</div>
-
-    case 2:
-      return <div>{props.leaders[0].name} : {props.leaders[0].score} points : {props.leaders[1].name} : {props.leaders[1].score} points
-      </div>
-
-    case 3:
-      return <div>{props.leaders[0].name} : {props.leaders[0].score} points : {props.leaders[1].name} : {props.leaders[1].score} points : {props.leaders[2].name} : {props.leaders[2].score} points
-      </div>
-
-    case 4:
-      return <div>{props.leaders[0].name} : {props.leaders[0].score} points : {props.leaders[1].name} : {props.leaders[1].score} points : {props.leaders[2].name} : {props.leaders[2].score} points : {props.leaders[3].name} : {props.leaders[3].score} points
-      </div>
-
-    default:
-      return <div>{props.leaders[0].name} : {props.leaders[0].score} points : {props.leaders[1].name} : {props.leaders[1].score} points : {props.leaders[2].name} : {props.leaders[2].score} points : {props.leaders[3].name} : {props.leaders[3].score} points :
-      {props.leaders[4].name} : {props.leaders[4].score} points
-      </div>
-
-  }
-}
-  // console.dir(props);
-  // const board = props.leaderss.map((element, index) => {
-  //   while (index < 5) {
-  //     return <div>{element.name}: {element.score} points</div>
-  //   }
-  // })
-  // return (
-  //    <div className="scroll-left">
-  //     <p>{board}</p>
-  //    </div>
-  //   )
-
-
-  console.log(props.leaders.length);
-//<p>  {generate_text(props)} </p>
-  return(
-
-
-  <div className="scroll-left">
-     <p>{generate_text()}</p>
-  </div>
-)
+  const board = props.leaders.map((element, index) => {
+    while (index < 10) {
+      return `${element.name}: ${element.score} points : `
+    }
+  })
+  return (
+     <div className="scroll-left">
+      <p>{board}</p>
+     </div>
+    )
 }
 
 export default Leaderboard;

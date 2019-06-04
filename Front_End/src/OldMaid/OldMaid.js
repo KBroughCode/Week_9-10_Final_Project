02d993ace8fb0785a67d5912ec9f3d836b4cc4b7
bsharp807 <a0030619@0030619s-MacBook-Pro.local>
2019-06-04 11:45:38 +0100

    reducer issue fixed, reduced deck mounting to store

--- Front_End/src/OldMaid/OldMaid.js
+++ Front_End/src/OldMaid/OldMaid.js
@@ -44,12 +44,13 @@ class OldMaid extends Component {
 const mapDispatchToProps = (dispatch) => ({
   getDeck() {
     dispatch(() => {
-      fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?cards=AS,AH,AC,AD,2S,2H,2D,2C,3S,3H,3D,3C,4S,4H,4C,4D,5S,5H,5C,5D,6S,6H,6D,6C,7S,7H,7D,7C,8D,8C,8S,8H,9D,9C,9S,9H,10D,10C,10S,10H,JD,JC,JH,JS,QS,KS,KH,KD,KC`)
+      fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?cards=AS,AH,AC,AD,2S,2H,2D,2C,3S,3H,3D,3C,4S,4H,4C,4D,5S,5H,5C,5D,6S,6H,6D,6C,7S,7H,7D,7C,8D,8C,8S,8H,9D,9C,9S,9H,0D,0C,0S,0H,JD,JC,JH,JS,QS,KS,KH,KD,KC`)
       .then(res => res.json())
       .then(deck => {
         fetch(`https://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=49`)
         .then(res => res.json())
         .then(deck => {
+          console.log(deck);
           dispatch({
             type: 'GET_OLD_MAID_DECK',
             deck
--- Front_End/src/reducers/index.js
+++ Front_End/src/reducers/index.js
@@ -4,7 +4,7 @@ import cardReducer from './card_reducer';
 import snapReducer from './snap_reducer';
 import coinReducer from './coin_reducer';
 import leaderBoardReducer from './leaderboard_reducer';
-import oldMaidReducer from './leaderboard_reducer';
+import oldMaidReducer from './old_maid_reducer';
 
 export default combineReducers({
   blackjack: cardReducer,

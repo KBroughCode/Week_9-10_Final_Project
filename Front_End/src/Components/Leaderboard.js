78a3d2401a72e7e4963eacc91ed7a2f0586c5f14
bsharp807 <a0030619@0030619s-MacBook-Pro.local>
2019-06-04 16:33:29 +0100

    removed console log from leaderboard

--- Front_End/src/Components/Leaderboard.js
+++ Front_End/src/Components/Leaderboard.js
@@ -3,7 +3,6 @@ import './leaderboard.css';
 //Top 10 leaderboard
 const Leaderboard = (props) => {
   const topTen = props.leaders.slice(0,10)
-  console.log(topTen);
   const highboard = topTen.map((element, index) => {
     return ` ♣ ${index+1}. ${element.name} - ₡${element.score} ♣ `
   })

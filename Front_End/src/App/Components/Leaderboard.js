28b6fd26aba69d6e4fa53c510ddda37bc33bd4d1
Jim <a9372275@9372275s-MacBook-Pro.local>
2019-06-28 15:44:51 +0100

    leaderboard now scrolls continuously

--- Front_End/src/App/Components/Leaderboard.js
+++ Front_End/src/App/Components/Leaderboard.js
@@ -1,24 +1,20 @@
 import React from 'react';
+import Ticker from 'react-ticker';
 import './leaderboard.css';
+
 //Top 10 leaderboard
 const Leaderboard = (props) => {
+
   const topTen = props.leaders.slice(0,10);
   const highboard = topTen.map((element, index) => {
     return ` ♣ ${index+1}. ${element.name} - ₡${element.score} ♣ `
   });
 
   return (
-    <div className="marquee">
-    <div>
-    <span><p>High Scores... {highboard}</p></span>
-    <span><p>High Scores... {highboard}</p></span>
-    </div>
-    </div>
+    <Ticker speed={20}>
+    {() => (<h1 style={{ whiteSpace: "nowrap" }}>High Scores...{highboard}</h1>)}
+    </Ticker>
    );
 }
 
 export default Leaderboard;
-
-// <div className="slideshow">
-// <p>High Scores {highboard} High Scores {highboard} High Scores {highboard}</p>
-// </div>

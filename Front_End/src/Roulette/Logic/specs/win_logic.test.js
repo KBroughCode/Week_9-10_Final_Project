2bb328573f8b076e002abf2d5b4fe6e4ec1ec505
Jim <a9372275@9372275s-MacBook-Pro.local>
2019-07-02 17:12:51 +0100

    draft test file completed for win logic

--- Front_End/src/Roulette/Logic/specs/win_logic.test.js
+++ Front_End/src/Roulette/Logic/specs/win_logic.test.js
@@ -6,68 +6,172 @@ describe('setupForJest', () => {
   }
   const aVariable = true;
 
-  test('player wins with bet on red when spin result is red', () => {
+// INSIDE BETS
+// STRAIGHT / SINGLE
+  test('player wins bet on single # when it equals the winning #', () => {
     expect(checkResult(arguments).result).toEqual('something');
   });
 
-  test('player loses with bet on red when spin result is black', () => {
+  test('player loses bet on single # when it NOT equals the winning #', () => {
     expect(checkResult(arguments).result).toEqual('something');
   });
 
-  test('player wins with bet on black when spin result is black', () => {
+  test('player wins bet on zero when it equals the winning #', () => {
     expect(checkResult(arguments).result).toEqual('something');
   });
 
-  test('player loses with bet on black when spin result is red', () => {
+  test('player loses bet on zero when it NOT equals the winning #', () => {
     expect(checkResult(arguments).result).toEqual('something');
   });
 
-  test('player wins with bet on evens when spin result is evens', () => {
+// OUTSIDE BETS
+// RED / BLACK
+  test('player wins bet on red when winning number is red', () => {
     expect(checkResult(arguments).result).toEqual('something');
   });
 
-  test('player loses with bet on evens when spin result is odds', () => {
+  test('player loses bet on red when winning number is black', () => {
     expect(checkResult(arguments).result).toEqual('something');
   });
 
-  test('player wins with bet on odds when spin result is odds', () => {
+  test('player loses bet on red when winning number is zero', () => {
     expect(checkResult(arguments).result).toEqual('something');
   });
 
-  test('player loses with bet on odds when spin result is evens', () => {
+  test('player wins bet on black when winning number is black', () => {
     expect(checkResult(arguments).result).toEqual('something');
   });
 
-  test('player wins with bet on low 12 when spin result is <= 12', () => {
+  test('player loses bet on black when winning number is red', () => {
     expect(checkResult(arguments).result).toEqual('something');
   });
 
-  test('player loses with bet on low 12 when spin result is > 12', () => {
+  test('player loses bet on black when winning number is zero', () => {
     expect(checkResult(arguments).result).toEqual('something');
   });
 
-
-
-
-  test('player wins with bet on red when spin result is red', () => {
+// EVEN / ODD
+  test('player wins bet on evens when winning number is evens', () => {
     expect(checkResult(arguments).result).toEqual('something');
   });
 
-  test('player wins with bet on red when spin result is red', () => {
+  test('player loses bet on evens when winning number is odds', () => {
     expect(checkResult(arguments).result).toEqual('something');
   });
 
-  test('player wins with bet on red when spin result is red', () => {
+  test('player loses bet on evens when winning number is zero', () => {
     expect(checkResult(arguments).result).toEqual('something');
   });
 
-  test('player wins with bet on red when spin result is red', () => {
+  test('player wins bet on odds when winning number is odds', () => {
     expect(checkResult(arguments).result).toEqual('something');
   });
 
-  test('player wins with bet on red when spin result is red', () => {
+  test('player loses bet on odds when winning number is evens', () => {
     expect(checkResult(arguments).result).toEqual('something');
   });
 
+  test('player loses bet on odds when winning number is zero', () => {
+    expect(checkResult(arguments).result).toEqual('something');
+  });
+
+// HIGH / LOW
+  test('player wins bet on low when winning number is low', () => {
+    expect(checkResult(arguments).result).toEqual('something');
+  });
+
+  test('player loses bet on low when winning number is high', () => {
+    expect(checkResult(arguments).result).toEqual('something');
+  });
+
+  test('player loses bet on low when winning number is zero', () => {
+    expect(checkResult(arguments).result).toEqual('something');
+  });
+
+  test('player wins bet on high when winning number is high', () => {
+    expect(checkResult(arguments).result).toEqual('something');
+  });
+
+  test('player loses bet on high when winning number is low', () => {
+    expect(checkResult(arguments).result).toEqual('something');
+  });
+
+  test('player loses bet on high when winning number is zero', () => {
+    expect(checkResult(arguments).result).toEqual('something');
+  });
+
+// DOZENS
+  test('player wins bet on 1st doz when winning number is in 1st doz', () => {
+    expect(checkResult(arguments).result).toEqual('something');
+  });
+
+  test('player loses bet on 1st doz when winning number is in 2nd/3rd', () => {
+    expect(checkResult(arguments).result).toEqual('something');
+  });
+
+  test('player loses bet on 1st doz when winning number is zero', () => {
+    expect(checkResult(arguments).result).toEqual('something');
+  });
+
+  test('player wins bet on 2nd doz when winning number is in 2nd doz', () => {
+    expect(checkResult(arguments).result).toEqual('something');
+  });
+
+  test('player loses bet on 2nd doz when winning number is in 1st/3rd', () => {
+    expect(checkResult(arguments).result).toEqual('something');
+  });
+
+  test('player loses bet on 2nd doz when winning number is zero', () => {
+    expect(checkResult(arguments).result).toEqual('something');
+  });
+
+  test('player wins bet on 3rd doz when winning number is in 3rd doz', () => {
+    expect(checkResult(arguments).result).toEqual('something');
+  });
+
+  test('player loses bet on 3rd doz when winning number is in 1st/2nd', () => {
+    expect(checkResult(arguments).result).toEqual('something');
+  });
+
+  test('player loses bet on 3rd doz when winning number is zero', () => {
+    expect(checkResult(arguments).result).toEqual('something');
+  });
+
+// COLUMNS
+  test('player wins bet on 1st column when winning # is in 1st column', () => {
+    expect(checkResult(arguments).result).toEqual('something');
+  });
+
+  test('player loses bet on 1st column when winning # is in 2nd/3rd', () => {
+    expect(checkResult(arguments).result).toEqual('something');
+  });
+
+  test('player loses bet on 1st column when winning # is zero', () => {
+    expect(checkResult(arguments).result).toEqual('something');
+  });
+
+  test('player wins bet on 2nd column when winning # is in 2nd column', () => {
+    expect(checkResult(arguments).result).toEqual('something');
+  });
+
+  test('player loses bet on 2nd column when winning # is in 1st/3rd', () => {
+    expect(checkResult(arguments).result).toEqual('something');
+  });
+
+  test('player loses bet on 2nd column when winning # is zero', () => {
+    expect(checkResult(arguments).result).toEqual('something');
+  });
+
+  test('player wins bet on 3rd column when winning # is in 3rd column', () => {
+    expect(checkResult(arguments).result).toEqual('something');
+  });
+
+  test('player loses bet on 3rd column when winning # is in 1st/2nd', () => {
+    expect(checkResult(arguments).result).toEqual('something');
+  });
+
+  test('player loses bet on 3rd column when winning # is zero', () => {
+    expect(checkResult(arguments).result).toEqual('something');
+  });
 
 })

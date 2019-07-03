b372efcd31ba4e5c304721ca1e2a71375b568a6d
Jim <a9372275@9372275s-MacBook-Pro.local>
2019-07-03 14:11:14 +0100

    ran 1st 2 jest tests

--- Front_End/src/Roulette/Logic/specs/win_logic.test.js
+++ Front_End/src/Roulette/Logic/specs/win_logic.test.js
@@ -2,7 +2,7 @@ const checkResult = require('../WinLogic');
 
 describe('setupForJest', () => {
   let winningNumber = null;
-  let board = [
+  let emptyBoard = [
     [{name: '1to18', value: 0}, {name: '19to36', value: 0}],
     [{name: '0', value: 0}, {name: '3', value: 0}, {name: '6', value: 0}, {name: '9', value: 0}, {name: '12', value: 0}, {name: '15', value: 0}, {name: '18', value: 0}, {name: '21', value: 0}, {name: '24', value: 0}, {name: '27', value: 0}, {name: '30', value: 0}, {name: '33', value: 0}, {name: '36', value: 0}, {name: 'row1', value: 0}],
     [{name: '2', value: 0}, {name: '5', value: 0}, {name: '8', value: 0}, {name: '11', value: 0}, {name: '14', value: 0}, {name: '17', value: 0}, {name: '20', value: 0}, {name: '23', value: 0}, {name: '26', value: 0}, {name: '29', value: 0}, {name: '32', value: 0}, {name: '35', value: 0}, {name: 'row2', value: 0}],
@@ -10,24 +10,24 @@ describe('setupForJest', () => {
     [{name: '1to12', value: 0}, {name: '13to24', value: 0}, {name: '25to36', value: 0}],
     [{name: 'even', value: 0}, {name: 'red', value: 0}, {name: 'black', value: 0}, {name: 'odd', value: 0}]
   ];
-  const reds = ['1','3','5','7','9','12','14','16','18','19','21','23','25','27','30','32','34','36'];
-  const blacks = ['2','4','6','8','10','11','13','15','17','20','22','24','26','28','29','31','33','35'];
-  const firstColumn = ['1','4','7','10','13','16','19','22','25','28','31','34'];
-  const secondColumn = ['2','5','8','11','14','17','20','23','26','29','32','35'];
-  const thirdColumn = ['3','6','9','12','15','18','21','24','27','30','33','36'];
+  let board = [];
 
-  test('player wins bet on 1st doz when winning number is in 1st doz', () => {
-    winningNumber = '23';
-    board[0][0].value = 5;
-    expect(checkResult(winningNumber, board)).toEqual(0);
+  beforeEach(() => {
+    board = [...emptyBoard];
+    for (let section of board) {
+      for (let bet of section) {
+        bet.value = 0
+      }
+    }
+    winningNumber = null;
   });
-
-
 
 // INSIDE BETS
 // STRAIGHT / SINGLE
-  xtest('player wins bet on single # when it equals the winning #', () => {
-    expect(checkResult(arguments).result).toEqual('something');
+  test('player wins bet on single # when it equals the winning #', () => {
+    winningNumber = '3';
+    board[1][1].value = 5;
+    expect(checkResult(winningNumber, board)).toEqual(180);
   });
 
   xtest('player loses bet on single # when it NOT equals the winning #', () => {
@@ -94,8 +94,10 @@ describe('setupForJest', () => {
   });
 
 // HIGH / LOW
-  xtest('player wins bet on low when winning number is low', () => {
-    expect(checkResult(arguments).result).toEqual('something');
+  test('player wins bet on low when winning number is low', () => {
+    winningNumber = '9';
+    board[0][0].value = 5;
+    expect(checkResult(winningNumber, board)).toEqual(10);
   });
 
   xtest('player loses bet on low when winning number is high', () => {

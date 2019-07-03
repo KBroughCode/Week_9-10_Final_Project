2b594d055956dd4e1417feff0c1578748b6bc484
Jim <a9372275@9372275s-MacBook-Pro.local>
2019-07-03 09:30:04 +0100

    improvements to win logic tests

--- Front_End/src/Roulette/Logic/specs/win_logic.test.js
+++ Front_End/src/Roulette/Logic/specs/win_logic.test.js
@@ -4,7 +4,11 @@ describe('setupForJest', () => {
   const anObject = {
     key: "value",
   }
-  const aVariable = true;
+  const reds = [1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36];
+  const blacks = [2,4,6,8,10,11,13,15,17,20,22,24,26,28,29,31,33,35];
+  const firstColumn = [1,4,7,10,13,16,19,22,25,28,31,34];
+  const secondColumn = [2,5,8,11,14,17,20,23,26,29,32,35];
+  const thirdColumn = [3,6,9,12,15,18,21,24,27,30,33,36];
 
 // INSIDE BETS
 // STRAIGHT / SINGLE
